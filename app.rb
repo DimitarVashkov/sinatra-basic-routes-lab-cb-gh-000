4d679ea83d049697ff97b5b731651e37e8adf229
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-02 14:55:37 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Dim"
+  end
+  get '/hometown' do
+    'My hometown is Tarnovo'
+  end
+  get '/favorite-song' do
+    'My favorite song is Back in Black'
+  end
 end
