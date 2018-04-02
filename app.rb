fae3a8467820ec8e1ef921e0235403a899a2ff38
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-02 14:55:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/name' { "My name is Dim" }
-  get '/hometown' { 'My hometown is Tarnovo'}
-  get '/favorite-song' {'My favorite song is Back in Black'}
+  get '/name' do
+    "My name is Dim"
+  end
+  get '/hometown' do
+    'My hometown is Tarnovo'
+  end
+  get '/favorite-song' do
+    'My favorite song is Back in Black'
+  end
 end
